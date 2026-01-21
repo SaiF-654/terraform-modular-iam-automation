1103e2b7451f754a72a5fabbec17b75d9b2eb826
Saif Ur Rehman <saifizaki456@gmail.com>
2026-01-21 20:18:57 +0000

    files added

--- main.tf
+++ main.tf
@@ -1,82 +1,85 @@
-# Provider
-provider "aws" {
-  region = "us-east-1"
-}
-
-############################################################
-# IAM GROUP
-############################################################
-
+# Create IAM Group
 resource "aws_iam_group" "group" {
-  name = "DevOpsGroup"
+  name = var.iam_group_name
 }
 
-# Attach policies to group directly
-resource "aws_iam_group_policy_attachment" "group_policies" {
-  for_each = {
-    "EC2FullAccess" = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
-    "S3FullAccess"  = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
-    "VPCFullAccess" = "arn:aws:iam::aws:policy/AmazonVPCFullAccess"
-  }
-
-  group      = aws_iam_group.group.name
-  policy_arn = each.value
-}
-
-############################################################
-# IAM USER
-############################################################
-
+# Create IAM User
 resource "aws_iam_user" "user" {
-  name = "devops-user"
+  name = var.iam_user_name
 }
 
-#Assign user to group
+# Add user to group
 resource "aws_iam_user_group_membership" "membership" {
   user   = aws_iam_user.user.name
   groups = [aws_iam_group.group.name]
 }
 
-# Optional Access Key
+# Optional: Create access key for IAM user
 resource "aws_iam_access_key" "access_key" {
   user = aws_iam_user.user.name
 }
 
-############################################################
-# IAM ROLE FOR EC2
-############################################################
-
+# Create IAM Role for EC2
 resource "aws_iam_role" "ec2_role" {
   name = "EC2Role"
 
   assume_role_policy = jsonencode({
-    Version = "2012-10-17"
+    Version = "2012-10-17",
     Statement = [{
-      Effect = "Allow"
-      Principal = {
-        Service = "ec2.amazonaws.com"
-      }
+      Effect = "Allow",
+      Principal = { Service = "ec2.amazonaws.com" },
       Action = "sts:AssumeRole"
     }]
   })
 }
 
-# Attach same policies to EC2 role
-resource "aws_iam_role_policy_attachment" "ec2_role_policies" {
-  for_each = {
-    "EC2FullAccess" = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
-    "S3FullAccess"  = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
-    "VPCFullAccess" = "arn:aws:iam::aws:policy/AmazonVPCFullAccess"
-  }
+# Least-privilege policy for EC2
+resource "aws_iam_policy" "ec2_custom_policy" {
+  name        = "EC2CustomPolicy"
+  description = "Least-privilege policy for EC2 instance"
 
-  role       = aws_iam_role.ec2_role.name
-  policy_arn = each.value
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "ec2:DescribeInstances",
+          "ec2:StartInstances",
+          "ec2:StopInstances",
+          "ec2:RebootInstances"
+        ],
+        Resource = "*"
+      },
+      {
+        Effect = "Allow",
+        Action = [
+          "ec2:DescribeVolumes",
+          "ec2:AttachVolume",
+          "ec2:DetachVolume"
+        ],
+        Resource = "*"
+      },
+      {
+        Effect = "Allow",
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
 }
 
-############################################################
-# INSTANCE PROFILE FOR EC2
-############################################################
+# Attach policy to EC2 role
+resource "aws_iam_role_policy_attachment" "ec2_custom_role_attachment" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = aws_iam_policy.ec2_custom_policy.arn
+}
 
+# Create instance profile for EC2
 resource "aws_iam_instance_profile" "ec2_instance_profile" {
   name = "EC2InstanceProfile"
   role = aws_iam_role.ec2_role.name
